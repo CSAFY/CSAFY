c0f9a64aa79ef09d1b121a3643b426279cabf79f
YoungUn Choi <darkstaryoung0406@gmail.com>
2022-04-27 02:22:02 +0900

    S06P12A102-28 #comment Feature: 일반 학습 상세 페이지

--- frontend/src/pages/StudyPage/StudyDetailPage.js
+++ frontend/src/pages/StudyPage/StudyDetailPage.js
@@ -1,31 +1,90 @@
+import { MyiFrame,
+  FullLayOut,
+  DetailLayOut,
+  TitleText,
+  StudyDetailDiv,
+  StudyDetailText,
+  StudyDetailHr,
+  FlexDiv,
+  RelatedQuestions
+ } from "./StudyDetailPage.styled"
+
 import { useEffect, useRef, useState } from "react";
 import { useRecoilValue } from "recoil";
 import { videoData } from "../../recoils";
 import styled, { css } from "styled-components";
+import ExampleCard from "../../components/atoms/studypage/ExampleCard"
 
 
 function StudyDetailPage() {
   const videoDatas = useRecoilValue(videoData)
   const Url = `https://www.youtube.com/embed/${videoDatas.videoId}`
+
+
+  const [quiz, setQuiz] = useState([])
+  const againQuiz = quiz.map((data) => 
+    
+      <ExampleCard
+        key={data}
+        >
+      </ExampleCard>
+  )
+
   return (
     <FullLayOut>
-      <MyiFrame
-        src={Url}
-      ></MyiFrame>
+      <DetailLayOut>
+        <TitleText>
+          {videoDatas.title}
+        </TitleText>
+        <MyiFrame
+          src={Url}
+        ></MyiFrame>
+
+        <StudyDetailHr></StudyDetailHr>
+
+        <StudyDetailDiv>
+          <StudyDetailText>
+            관련 모의고사
+          </StudyDetailText>
+
+          <FlexDiv>
+            <ExampleCard>
+
+            </ExampleCard>
+            <ExampleCard>
+
+            </ExampleCard>
+            <ExampleCard>
+
+            </ExampleCard>
+          </FlexDiv>
+          
+        </StudyDetailDiv>
+
+        <StudyDetailHr></StudyDetailHr>
+
+        <StudyDetailDiv>
+          <StudyDetailText>
+            관련 질문
+          </StudyDetailText>
+          <RelatedQuestions>
+            OSI 7계층에 대해 설명해보세요.
+          </RelatedQuestions>
+          <RelatedQuestions>
+            OSI 7계층에 대해 설명해보세요.
+          </RelatedQuestions>
+          <RelatedQuestions>
+            OSI 7계층에 대해 설명해보세요.
+          </RelatedQuestions>
+          <RelatedQuestions>
+            OSI 7계층에 대해 설명해보세요.
+          </RelatedQuestions>
+
+        </StudyDetailDiv>
+      </DetailLayOut>
     </FullLayOut>
   
   )
 }
 export default StudyDetailPage
 
-const MyiFrame = styled.iframe`
-  
-  width : 700px;
-  height : 394px;
-  allowfullscreen
-  src : ${(props) => props.videoId}
-`
-
-const FullLayOut = styled.div`
-  height : 100vh;
-`
