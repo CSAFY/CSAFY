34de838007a85e219521334bfe8be85bb6558e36
YoungUn Choi <darkstaryoung0406@gmail.com>
2022-05-04 18:48:34 +0900

    S06P12A102-48 #comment Fix: OX퀴즈 학습 마무리

--- frontend/src/components/atoms/intensivePage/OXquiz.js
+++ frontend/src/components/atoms/intensivePage/OXquiz.js
@@ -1,108 +1,207 @@
 import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
-
-import axios from 'axios';
-import { useRecoilState, useRecoilValue } from "recoil";
-import { shortAnswerData } from "../../../recoils";
-
 import Box from '@mui/material/Box';
-import Alert from '@mui/material/Alert';
-import IconButton from '@mui/material/IconButton';
-import Collapse from '@mui/material/Collapse';
+import { useTheme } from '@mui/material/styles';
+import MobileStepper from '@mui/material/MobileStepper';
 import Button from '@mui/material/Button';
-import CloseIcon from '@mui/icons-material/Close';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+
+import { useRecoilState } from "recoil";
+import { oxquizData } from "../../../recoils";
+
 
 function OXquiz() {
-  const [shortAData, setShortAData] = useRecoilState(shortAnswerData)
-
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = useState(0);
   
+  const [oxData, setOXData] = useRecoilState(oxquizData)
+  const maxSteps = oxData.length;
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    setSelectO(0)
+    setSelectX(0)
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    setSelectO(0)
+    setSelectX(0)
+  };
+
   useEffect(() => {
-    setShortAData({ 
+    setOXData([{ 
       explanation : "단어에 대한 설명을 제시해 줍니다 . 한줄일지 두줄일지 모르고 여튼...",
-      answer : "정답"})
+      answer : 2},
+      { 
+        explanation : "단어에 대한 설명을 제시해 줍니다 . 모르고 여튼...",
+        answer : 1}
+    ])
   }, [])
 
-  const [inputAnswerData, setInputAnswerData] = useState()
-  const onChanges = (event) => {
-    
-    setInputAnswerData(event.target.value)
+  const [selectO, setSelectO] = useState(0)
+  const [selectX, setSelectX] = useState(0)
+
+
+  const onClickO = () => {
+    setSelectO(1)
+    setSelectX(0)
   }
-  const [open, setOpen] = useState(false);
+  const onClickX = () => {
+    setSelectO(0)
+    setSelectX(2)
+  }
+
+  const OXCardPack = (
+    <FlexDiv>
+      
+      <CardCoverDiv>
+        
+        <OXCard onClick={onClickO}
+          border={selectO === 0 ?  "solid 2px #ebeef4;" 
+                : selectO === oxData[activeStep].answer ? "solid 2px #25b26d;" 
+                : "solid 2px  #e12e2e;"}
+          backgroundColor={selectO === 0 ?  "#fff;" 
+          : selectO === oxData[activeStep].answer ? "#f2fbf6;"
+          : "#FFD5D2;"}>
+          <O>
+            O
+          </O>
+        </OXCard>
+        {selectO === 0 ?  " " 
+          : selectO === oxData[activeStep].answer ? "정답입니다" 
+          : "틀렸습니다"}
+      </CardCoverDiv>
+      <CardCoverDiv>
+        
+        <OXCard onClick={onClickX}
+        border={selectX === 0 ?  "solid 2px #ebeef4;" 
+        : selectX === oxData[activeStep].answer ? "solid 2px #25b26d;" 
+        : "solid 2px  #e12e2e;"}
+        backgroundColor={selectX === 0 ?  "#fff;" 
+        : selectX === oxData[activeStep].answer ? "#f2fbf6;"
+        : "#FFD5D2;"}>
+          <X>
+            X
+          </X>
+        </OXCard>
+        {selectX === 0 ?  " " 
+          : selectX === oxData[activeStep].answer ? "정답입니다" 
+          : "틀렸습니다"}
+      </CardCoverDiv>
+    </FlexDiv>
+  )
   
   return(
-    <OXquizCardDiv>
-      <MeaningDiv>
-        {shortAData.explanation}
+    <Box  sx={{  flexGrow: 1 , margin: "10px 20px 10px 20px", 
+    borderRadius: "20px",
+    boxShadow: "0 0 15px 0 rgba(0, 0, 0, 0.2)",
+    backgroundColor: "#fff"}}>
+    
+    <MeaningDiv>
+        {oxData[activeStep].explanation}
       </MeaningDiv>
-      
-      
-      <AnswerInput
-      placeholder="정답을 입력해 주세요"
-      value={inputAnswerData}
-      onChange={onChanges}>
-      </AnswerInput>
-      
-      <Box sx={{ width: '100%' }}>
-      <Collapse in={open}>
-        
-        <Alert
-          severity= {shortAData.answer === inputAnswerData? "success" : "error" }
-          action={
-            <IconButton
-              aria-label="close"
-              color="inherit"
-              size="small"
-              onClick={() => {
-                setOpen(false);
-              }}
-            >
-              <CloseIcon fontSize="inherit" />
-            </IconButton>
-          }
-          sx={{ mb: 2 }}
+      {OXCardPack}
+      <FootBar></FootBar>
+    <MobileStepper
+      variant="text"
+      steps={maxSteps}
+      position="static"
+      activeStep={activeStep}
+      nextButton={
+        <Button
+          size="small"
+          onClick={handleNext}
+          disabled={activeStep === maxSteps - 1}
         >
-          Close me!
-        </Alert>
-      </Collapse>
-      <Button
-        disabled={open}
-        variant="outlined"
-        onClick={() => {
-          setOpen(true);
-        }}
-      >
-        정답 확인
-      </Button>
-      
-    </Box>
-      
-    </OXquizCardDiv>
-
+          Next
+          {theme.direction === 'rtl' ? (
+            <KeyboardArrowLeft />
+          ) : (
+            <KeyboardArrowRight />
+          )}
+        </Button>
+      }
+      backButton={
+        <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+          {theme.direction === 'rtl' ? (
+            <KeyboardArrowRight />
+          ) : (
+            <KeyboardArrowLeft />
+          )}
+          Back
+        </Button>
+      }
+    />
+    
+  </Box>
   )
 }
 export default OXquiz
 
-const OXquizCardDiv = styled.div`
-  width : 90%;
-  height : 530px;
-  align-Items: 'center';
-  margin : 10px 20px 10px 20px;
-  border : solid;
-  justify-Content: "center";
-`
 
 const MeaningDiv = styled.div`
   width : 90%;
-  height : 50%;
-  margin : 40px auto 0 auto;
+  height : 240PX;
+  margin : 20px auto 0 auto;
   text-align: left;
 `
 
-const AnswerInput = styled.input`
-  width : 250px;
-  height : 30px;
+const FlexDiv = styled.div`
+  display: flex;
+  align-content: center;
+  justify-Content: "center";
+`
+const CardCoverDiv = styled.div`
+  margin: 0 auto 0 auto;
+  height: 174px;
+`
+const OXCard = styled.div`
+  width: 230px;
+  height: 154px;
+  flex-grow: 0;
+  
+  padding: 0 0.1px 0 0;
   border-radius: 11px;
-  border: solid 2px #ebeef4;
-  padding : 15px;
+  
+  border: ${(props) => props.border}
+  background-color: ${(props) => props.backgroundColor}
 `
 
+const O = styled.span`  
+  width: 95px;
+  height: 88px;
+  font-family: SUIT;
+  font-size: 96px;
+  font-weight: 600;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  text-align: left;
+  color: #e12e2e;
+`
+
+const X = styled.span`  
+  width: 95px;
+  height: 88px;
+  margin: 0 0 36px;
+  font-family: SUIT;
+  font-size: 96px;
+  font-weight: 600;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  text-align: left;
+  color: #1c45d8;
+`
+
+const FootBar = styled.div`
+  width: 100%;
+  height: 8px;
+  flex-grow: 0;
+  margin: 42px 0 0;
+  background-color: #d7e4ec;
+`
