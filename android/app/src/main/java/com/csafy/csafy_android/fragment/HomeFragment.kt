b290bad2211f1f8f8475beb46bfc1184094b4dac
최하영 <devhapeng@gmail.com>
2022-05-09 16:10:34 +0900

    merge mingu

--- android/app/src/main/java/com/csafy/csafy_android/fragment/HomeFragment.kt
+++ android/app/src/main/java/com/csafy/csafy_android/fragment/HomeFragment.kt
@@ -1,14 +1,14 @@
-package com.example.csafy_android.fragment
+package com.csafy.csafy_android.fragment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebChromeClient
 import android.webkit.WebView
-import androidx.databinding.DataBindingUtil.setContentView
-import com.example.csafy_android.R
-import com.example.csafy_android.databinding.ActivityLoginBinding
+import android.webkit.WebViewClient
 import com.example.csafy_android.databinding.FragmentHomeBinding
 
 
@@ -25,6 +25,7 @@ class HomeFragment : Fragment() {
 //
 //    }
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -34,8 +35,12 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
 
         webView = binding.webMain
+        webView.webViewClient = WebViewClient()  // 앱 내부에서 웹을 보여줌.
+        webView.webChromeClient = WebChromeClient()
+        webView.settings.useWideViewPort = true // 화면 사이즈 맞추기 허용 여부
         webView.settings.javaScriptEnabled = true
-        webView.loadUrl("https://www.naver.com")
+        webView.settings.domStorageEnabled = true
+        webView.loadUrl("https://www.google.com")
 
         return view
 
--- android/app/src/main/java/com/csafy/csafy_android/fragment/TestOXFragment.kt
+++ android/app/src/main/java/com/csafy/csafy_android/fragment/TestOXFragment.kt
@@ -8,7 +8,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.databinding.DataBindingUtil.setContentView
 import com.example.csafy_android.R
 import com.example.csafy_android.activity.MainActivity
 import com.example.csafy_android.databinding.FragmentTestOXBinding
