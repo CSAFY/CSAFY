2fec44ac14fe2f405846eefc574cf3d9a8c4530e
YoungUn Choi <darkstaryoung0406@gmail.com>
2022-05-19 05:43:14 +0900

    S06P12A102-170 #comment Fix :섬네일 컴포넌트와 과목 변경 버튼  디자인 변경 0518
    Former-commit-id: 7fb5f586615df5580a06306ac311b3cc677e808e

--- frontend/src/components/atoms/studypage/CategoryList.js
+++ frontend/src/components/atoms/studypage/CategoryList.js
@@ -4,62 +4,85 @@ import { useEffect, useRef, useState } from "react";
 import { createTheme } from '@mui/material/styles';
 import { useRecoilValue } from "recoil";
 import { studyData } from "../../../recoils";
-import styled from "styled-components";
+import styled, { css }  from "styled-components";
+
+// import { createTheme } from '@mui/material/styles';
 
 
 function CategoryList(props) {
-  const [alignment, setAlignment] = useState('전체');
+  const [alignment, setAlignment] = useState('전체 학습');
   const studyDatas = useRecoilValue(studyData)
 
-  const handleAlignment = (event, newAlignment) => {
-    if(newAlignment !== null){
-      setAlignment(newAlignment);
-      props.selectKategorie(newAlignment)
-    }
-  };
-  
-  const btncolors = ['standard' , 'primary' , 'secondary' , 'error' , 'info' , 'success' , 'warning']
+  // const handleAlignment = (event, newAlignment) => {
+  //   if(newAlignment !== null){
+  //     setAlignment(newAlignment);
+  //     props.selectKategorie(newAlignment)
+  //   }
+  // };
 
-  const againToggleButton = props.categori.map((data, index) => 
-    
-    <ToggleButton
-      key={index}
-      value={data}
-      color={btncolors[index % 7]}
-      sx={{
-        width: "180px;",
-        fontSize: "17px;",
-        fontFamily: "SUIT;",
-        fontWeight: "600;",
-        display:"flex",
-        justifyContent: "space-between;",
-        boxShadow: "0 0 4px 0 rgba(0, 0, 0, 0.2);",
-        borderRadius: "15px;",
-        backgroundColor: "#fff;",
-        height: "60px;",
-        border: "solid 1px rgba(0,0,0,0.08);",
-        margin:"0 0 10px 0;"
-        
-        }}
-      >
-      
-      <span>{data}</span> 
-      <TextDiv>{data === "전체" ? studyDatas.length : studyDatas.filter(element => data === element.categoryId).length}</TextDiv>
-    </ToggleButton>
-    
+  // const againToggleButton = props.categori.map((data, index) => 
+  //   <ToggleButton
+  //     className='ToggleButton'
+  //     key={index}
+  //     value={data}
+  //     // color= {theme}
+  //     // disableFocusRipple= {true}
+  //     // disableRipple = {true}
+  //     sx={{
+  //       width: "180px;",
+  //       fontSize: "17px;",
+  //       fontFamily: "SUIT;",
+  //       fontWeight: "600;",
+  //       display:"flex",
+  //       justifyContent: "space-between;",
+  //       boxShadow: "0 0 4px 0 rgba(0, 0, 0, 0.2);",
+  //       borderRadius: "15px;",
+  //       borderTopLeftRadius: "30px;",
+  //       height: "60px;",
+  //       border: "solid 1px rgba(0,0,0,0.08);",
+  //       margin:"0 0 10px 0;"
+  //       }}
+  //     >
+  //     <span>{data}</span> 
+  //     <TextDiv>{data === "전체" ? studyDatas.length : studyDatas.filter(element => data === element.categoryId).length}</TextDiv>
+  //   </ToggleButton>
+  // )
+
+  const onClickBTN = (data) => {
+    console.log(data)
+    setAlignment(data)
+    props.selectKategorie(data)
+  }
+
+  const toggle = props.categori.map((data, index) => 
+      <ToggleBtn 
+        key={index}
+        onClick={() => onClickBTN(data)}
+        able={data === alignment ? "Y" : "N"}>
+        <span>{data}</span> 
+        <TextDiv>{data === "전체 학습" ? 
+          studyDatas.length 
+          : studyDatas.filter(element => data === element.categoryId).length}
+        </TextDiv>
+      </ToggleBtn>
   )
+
+
   return (
-    <ToggleButtonGroup
-      orientation="vertical"
-      value={alignment}
-      exclusive
-      onChange={handleAlignment}
-      aria-label="text alignment"
-      color="primary"
-      sx={{margin: "10px 0 0 0;", width: "180px;"}}
-    >
-      {againToggleButton}
-    </ToggleButtonGroup>
+    // <ToggleButtonGroup
+    //   orientation="vertical"
+    //   value={alignment}
+    //   exclusive
+    //   onChange={handleAlignment}
+    //   aria-label="text alignment"
+      
+    //   sx={{margin: "10px 0 0 0;", width: "180px;"}}
+    // >
+    //   {againToggleButton}
+    // </ToggleButtonGroup>
+    <div>
+      {toggle}
+    </div>
   );
 }
 export default CategoryList;
@@ -68,5 +91,39 @@ const TextDiv = styled.div`
   width: 45px;
   background-color: #E0EFF8;
   border-radius: 10px;
-  color : dimgray;
+  font-weight : 0;
+  font-size : 14px;
+`
+const ToggleBtn = styled.button` 
+  width : 180px;
+  font-size : 17px;
+  font-family : SUIT;
+  font-weight : 600;
+  display : flex;
+  justify-content : space-between;
+  border-radius: 11px;
+  height : 60px;
+  margin : 0 0 10px 0;
+  align-items: center;
+  background-color: #f4fbfe;
+  border: none;
+  
+
+  ${(props) => {
+    if (props.able === "Y") {
+      return css`
+        {
+          background-color: #42A7E8;
+          color: #fff;
+          ${TextDiv}{
+            background-color: #008ED0;
+          }
+        }
+      `;
+    } else {
+      return css`
+        cursor: default;
+      `;
+    }
+  }}
 `
