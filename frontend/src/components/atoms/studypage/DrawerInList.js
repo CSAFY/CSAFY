81d01530b20445def05073cab1cfb3b013f55726
YoungUn Choi <darkstaryoung0406@gmail.com>
2022-05-19 15:19:07 +0900

    S06P12A102-170 #comment Fix: 사이드바 디자인 완성
    Former-commit-id: d8723ae2011ba3ebbf3200fe99cf7eff100f9083

--- frontend/src/components/atoms/studypage/DrawerInList.js
+++ frontend/src/components/atoms/studypage/DrawerInList.js
@@ -16,7 +16,11 @@ import styled, {css} from "styled-components";
 import CheckCircleOutlineSharpIcon from '@mui/icons-material/CheckCircleOutlineSharp';
 import StarIcon from '@mui/icons-material/Star';
 
+import CusStudyLinearWithValueLabel from "./CusStudyLinearWithValueLabel"
+
 import { useEffect, useRef, useState } from "react";
+
+import Tooltip from '@mui/material/Tooltip';
 
 
 export default function DrawerInList(props) {
@@ -37,114 +41,113 @@ export default function DrawerInList(props) {
 
   const ListItems = (categoryId) =>  {
     return(
-      studyDatas.slice(1).map((data, index) => {
-        if (categoryId === data.categoryId){
-          return(
-            <CusListItem button  key={index} onClick={(event) => {onClickVideo(event, {
-              "imgSrc":`https://i.ytimg.com/vi/${data.videoId}/hqdefault.jpg`,
-              "title": data.title,
-              "videoId":data.videoId,
-              "category2Id" : data.category2Id,
-              "categoryId": data.categoryId,
-              "favorites" : data.favorites,
-              "id" : data.id,
-              "seen" : data.seen })}} 
-              // sx={{ background: "#F5F5F5;",
-              //  border: "solid 1px;",
-              //  borderRadius: "15px;",
-              //  marginTop: "5px;",
-              //  margin:"10px 0 0 0;",
-              //  display:" flex;",
-              //  justifyContent: "space-between;"}}
-              >
-              {data.title} 
-              <span>
-              {data.seen === 1 ? 
-                <CheckCircleOutlineSharpIcon 
-                  color="success" 
-                  sx={{width:`20px;`, height:`20px;`}}>
-                </CheckCircleOutlineSharpIcon>: null}
-              {data.favorites === 1 ? 
-                <StarIcon color="warning" 
-                sx={{width:`20px;`, height:`20px;`}} 
-                ></StarIcon> : null}
-              </span>
-            </CusListItem>
-          )
-        }
+      studyDatas.filter(element => categoryId === element.categoryId).map((data, index) => {
+        return(
+        
+          <CusListItem button  key={index} onClick={(event) => {onClickVideo(event, {
+            "imgSrc":`https://i.ytimg.com/vi/${data.videoId}/hqdefault.jpg`,
+            "title": data.title,
+            "videoId":data.videoId,
+            "category2Id" : data.category2Id,
+            "categoryId": data.categoryId,
+            "favorites" : data.favorites,
+            "id" : data.id,
+            "seen" : data.seen })}} 
+            >
+            <Tooltip title={data.title} placement="right-end"  sx={{fontSize: "35px"}}>
+              <Contents>
+                {index + 1}. {data.title} 
+              </Contents>
+            </Tooltip>
+            
+            <span style={{display: "flex", height: "100%", alignItems: "center", marginRight: "10px"}}>
+            {data.seen === 1 ? 
+              <img src="images/check.png" alt="check"
+                style={{width:`17px`, height:`17px`}}>
+              </img>: null}
+            {data.favorites === 1 ? 
+              <img src="images/star.png" alt="star"
+              style={{width:`17px`, height:`17px`}} 
+              ></img> : null}
+            </span>
+          </CusListItem>
+        )
       })
     )
   }
   
   
-  const DrawerInList = props.data.slice(1).map((categoryId, index) => 
-    <Accordion 
-      expanded={expanded === `panel${index + 1}`} 
-      onChange={handleChange(`panel${index + 1}`)}
-      key ={index}
-      sx={{ margin: "0 auto 0 auto;", 
-      backgroundColor: "rgba(0,0,0,0);",
-      boxShadow: "0px 0px 0px 0px rgb(0 0 0 / 0%);"}}>
-      <AccordionSummary
+  // const DrawerInList = props.data.slice(1).map((categoryId, index) => 
+  //   <Accordion 
+  //     expanded={expanded === `panel${index + 1}`} 
+  //     onChange={handleChange(`panel${index + 1}`)}
+  //     key ={index}
+  //     sx={{ margin: "0 auto 0 auto;", 
+  //     backgroundColor: "rgba(0,0,0,0);",
+  //     boxShadow: "0px 0px 0px 0px rgb(0 0 0 / 0%);"}}>
+  //     <AccordionSummary
         
-        aria-controls={`panel${index + 1}bh-content`}
-        id={`panel${index + 1}bh-header`}
-        sx={{ margin:"10px 20px 0 20px;",borderRadius: "15px;",
-        border: "solid 1px;",
-        background: "#F5F5F5;",
-        }}
-      >
-        <Typography sx={{ width: "90%;", flexShrink: 0,  display:"flex",
-          justifyContent: "space-between;",fontWeight: "600;",}}>
-          <span>
-            {categoryId} 
-          </span>
-          <TextDiv>
-            {studyDatas.filter(element => categoryId === element.categoryId).length}
-          </TextDiv>
-        </Typography>
-      </AccordionSummary>
-      <AccordionDetails
-        sx={{ overflowY : "scroll;", maxHeight: "500px;", margin: "0 10px 0 20px;"}}>
+  //       aria-controls={`panel${index + 1}bh-content`}
+  //       id={`panel${index + 1}bh-header`}
+  //       sx={{ margin:"10px 20px 0 20px;",borderRadius: "15px;",
+  //       border: "solid 1px;",
+  //       background: "#F5F5F5;",
+  //       }}
+  //     >
+  //       <Typography sx={{ width: "90%;", flexShrink: 0,  display:"flex",
+  //         justifyContent: "space-between;",fontWeight: "600;",}}>
+  //         <span>
+  //           {categoryId} 
+  //         </span>
+  //         <CntDiv>
+  //           {studyDatas.filter(element => categoryId === element.categoryId).length}
+  //         </CntDiv>
+  //       </Typography>
+  //     </AccordionSummary>
+  //     <AccordionDetails
+  //       sx={{ overflowY : "scroll;", maxHeight: "500px;", margin: "0 10px 0 20px;"}}>
         
-        {ListItems(categoryId)}
+  //       {ListItems(categoryId)}
         
-      </AccordionDetails>
-    </Accordion>
-  )
-
-  const [numClick, setNumClick] = useState("")
-
-  useEffect(() => {
-    console.log(props)
-  }, [])
-
-  const ppp = (categoryId) => {
-    setNumClick(categoryId)
-    console.log("numClick",numClick)
-    console.log("categoryId", categoryId )
-    console.log("categoryId === numClick", categoryId === numClick)
+  //     </AccordionDetails>
+  //   </Accordion>
+  // )
+  
+  const onClickBtn = (data) => {
+    
+    setNowCatego(data)
   }
 
-  const Pppp = props.data.slice(1).map((categoryId, index) => 
-    <Cont 
-      key={index}
-      status = {categoryId === numClick ? "N" : "Y"}
-      >
-       
-      <BeforAccordion  onClick={() => ppp("")}>
-        <CusSpanText>
-          {categoryId} 
-        </CusSpanText>
-        <TextDiv>
-          {studyDatas.filter(element => categoryId === element.categoryId).length}
-        </TextDiv>
-      </BeforAccordion> 
-       <AfterAccordion onClick={() => ppp(categoryId)}>
-        {categoryId} 
-      </AfterAccordion>
+  const [nowCatego, setNowCatego] = useState('')
+  const AccordionDiv = props.data.slice(1).map((categoryId, index) => 
+    <Cont key={index}>
+      <Cdiv able={nowCatego === categoryId? true : false}>
+        {nowCatego === categoryId ?
+        <Opened onClick={()=> onClickBtn('')}>
+          <OpenedCateTitle>
+           {categoryId}
+          </OpenedCateTitle>
+          <CusStudyLinearWithValueLabel 
+            selectAnswerCNT={studyDatas.filter(element => categoryId === element.categoryId).filter(element => 1 === element.seen).length}
+            maxSteps={studyDatas.filter(element => categoryId === element.categoryId).length}></CusStudyLinearWithValueLabel>
+          <OpenedCateParsent>
+            {Math.round((studyDatas.filter(element => categoryId === element.categoryId).filter(element => 1 === element.seen).length / studyDatas.filter(element => categoryId === element.categoryId).length) * 100) } % 수강 완료
+          </OpenedCateParsent>
+        </Opened>
+        :<Closed onClick={()=> onClickBtn(categoryId)}>
+          <CategoryText  style={{width:"110px", textAlign: "center"}}>
+            {categoryId}
+          </CategoryText>
+          <CntDiv>
+            {studyDatas.filter(element => categoryId === element.categoryId).length}
+          </CntDiv>
+        </Closed>
+        }
+      </Cdiv>
       
-
+      <AccordionItems able={nowCatego === categoryId? true : false}>
+        {ListItems(categoryId)}
+      </AccordionItems>
       
     </Cont>
 
@@ -152,69 +155,14 @@ export default function DrawerInList(props) {
   
   return (
     <div>
-      {/* {DrawerInList} */}
-      {Pppp}
+      {AccordionDiv}
     </div>
   );
 }
 
-
-
-const TextDiv = styled.div`  
-  width: 26px;
-  height: 23px;
-  background-color: #E0EFF8;
-  border-radius: 10px;
+const CategoryText = styled.span`  
+  width: 110px; 
   text-align: center;
-  font-family: SUIT;
-  font-size: 15px;
-  font-weight: 600;
-  font-stretch: normal;
-  font-style: normal;
-  line-height: normal;
-  letter-spacing: normal;
-  text-align: center;
-  color: #404040;
-  padding-top: 3px;
-`
-const CusListItem = styled.div`  
-  background: #F5F5F5;
-  border : solid 1px;
-  margin-top:  5px;
-  margin: 10px 0 0 0;
-  display: flex;
-  justify-content: space-between;
-`
-const BeforAccordion = styled.div` 
-  width : 180px;
-  height : 40px;
-  margin : 0 auto 15px auto;
-  background-color : #FFFFFF;
-  border-radius: 11px;
-  box-shadow: 0 0 7px 0 rgba(39, 110, 232, 0.15);
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding : 10px 10px 10px 10px;
-  transition : 1s;
-`
-
-const AfterAccordion = styled.div` 
-  width : 180px;
-  height : 90px;
-  margin : 0 auto 15px auto;
-  background-color : #FFFFFF;
-  border-radius: 11px;
-  box-shadow: 0 0 7px 0 rgba(39, 110, 232, 0.15);
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding : 10px 10px 10px 10px;
-  transition: margin 150ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
-`
-
-
-const CusSpanText = styled.span`  
   font-family: SUIT;
   font-size: 16px;
   font-weight: 600;
@@ -223,21 +171,129 @@ const CusSpanText = styled.span`
   line-height: normal;
   letter-spacing: normal;
   text-align: left;
-  color: #000;
 `
 
 
-const Cont = styled.div`
+const CntDiv = styled.div`  
+  width: 30px;
+  height: 30px;
+  background-color: #E0EFF8;
+  border-radius: 10px;
+  font-weight : 0;
+  font-size : 14px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  font-family: SUIT;
+  font-size: 13px;
+  font-weight: 600;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
   
-  height: 200px;
-  background-color: #f4fbfe;
-  transition-duration: 0.5s
+  color: #404040;
+`
+const CusListItem = styled.div`  
+  height: 35px;
+  margin-top:  5px;
+  margin: 10px 0 0 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+`
+
+
+const Opened = styled.div` 
+  
+  width : 80%;
+
+
+  height: 90px;
+  
+  background: #F5F5F5;
+  backface-visibility : hidden;
+  margin: 10px 26px 0 26px;
+  border-radius: 11px;
+  box-shadow: 0 0 7px 0 rgba(39, 110, 232, 0.15);
+  background-color: #fff;
+`
+
+const Closed = styled.div` 
+  display : flex;
+  align-items : center;
+  justify-content: space-around;
+  width : 80%;
+  
+  
+  height: 54px;
+  
+  background: #F5F5F5;
+  backface-visibility : hidden;
+  margin: 10px 26px 0 26px;
+  border-radius: 11px;
+  box-shadow: 0 0 7px 0 rgba(39, 110, 232, 0.15);
+  background-color: #fff;
+`
+
+const Cdiv = styled.div`  
+  width : 100%;
+  
   ${(props) => {
-    if (props.status === "Y") {
+    if ( props.able === true){
+      return css` 
+        {
+          margin: 20px auto 0 auto;
+          height : 20px;
+          ${Opened} {
+            display : visible;
+            position : absolute
+          }
+          ${Closed} {
+            display : hidden;
+            position : absolute
+          }
+        }
+      `
+    } else {
       return css`
         {
-          background-color: #42A7E8;
-          color: #fff;
+          margin: 20px auto 0 auto;
+          height : 40px;
+          ${Opened} {
+            display : hidden;
+            position : absolute
+          }
+          ${Closed} {
+            display : visible;
+            position : absolute
+          }
+        }
+      `;
+    }
+  }}
+
+`
+
+
+const AccordionItems = styled.div` 
+  width : 76%;
+  height : 0;
+  margin: 15px 26px 0 26px;
+  padding :5px 00px 0 10px;
+  background-color: #fff;
+  overflow-y: scroll;
+  border-radius: 11px;
+  background-color: #fff;
+  transition: height 0.35s ease;
+  ${(props) => {
+    if (props.able === true) {
+      return css`
+        {
+          height : 400px;
+          padding-top : 80px;
         }
       `;
     } else {
@@ -248,11 +304,74 @@ const Cont = styled.div`
   }}
 `
 
+const Contents = styled.div`
+  width: 120px;  
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`
+
+const OpenedCateTitle = styled.div`
+  margin-top: 15px;  
+  font-family: SUIT;
+  font-size: 19px;
+  font-weight: 600;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  text-align: center;
+  color: #000;
+`
+
+const OpenedCateParsent = styled.div`  
+  margin-top: 5px;
+  font-family: SUIT;
+  font-size: 16px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  text-align: center;
+  color: #000;
+`
+
+
+
+const Cont = styled.div`
+  display : flex;
+  posotion : relative;
+  flex-direction : column;
+  justify-content : center;
+`
+
+
+
+
+// height: 200px;
+//   background-color: #f4fbfe;
+//   transition-duration: 0.5s
+//   ${(props) => {
+//     if (props.status === "Y") {
+//       return css`
+//         {
+//           background-color: #42A7E8;
+//           color: #fff;
+//         }
+//       `;
+//     } else {
+//       return css`
+//         cursor: default;
+//       `;
+//     }
+//   }}
+
 // ${(props) => {
 //   if ( props.status === "Y"){
 //     return css` 
 //       {
-//         ${BeforAccordion} {
+//         ${Opened} {
 //           display : hidden;
 //           position : absolute
 //         }
@@ -265,7 +384,7 @@ const Cont = styled.div`
 //   } else {
 //     return css`
 //       {
-//         ${BeforAccordion} {
+//         ${Opened} {
 //           display : visible;
 //           position : absolute
 //         }
@@ -277,7 +396,7 @@ const Cont = styled.div`
 //     `;
 //   }
 // }}
-// ${BeforAccordion} {
+// ${Opened} {
 //   position : absolute;
 //   display : visible;
 // }
